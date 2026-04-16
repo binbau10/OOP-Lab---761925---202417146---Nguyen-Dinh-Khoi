56128082931f3df876091158291fbd34e401c981
binbau10 <khoind2006@gmail.com>
2026-04-16 13:27:00 +0700

    nothing here

--- Lab02/TestPassingParameter.java
+++ Lab02/TestPassingParameter.java
@@ -8,9 +8,6 @@ public class TestPassingParameter{
         changeTitle(jungleDVD, cinderellaDVD.getTitle());
         System.out.println("Jungle dvd title: " + jungleDVD.getTitle());
     }
-    public void setTitle(String title){
-        this.title = title;
-    }
     public static void swap(Object o1, Object o2){
         Object tmp = o1;
         o1 = o2;

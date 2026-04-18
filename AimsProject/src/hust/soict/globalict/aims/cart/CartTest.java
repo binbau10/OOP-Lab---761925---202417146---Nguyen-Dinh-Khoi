70c4ae474c3e3766ae4bc137d3dbb929cf8ee0ab
binbau10 <khoind2006@gmail.com>
2026-04-18 21:55:11 +0700

    poly

--- AimsProject/src/hust/soict/globalict/aims/cart/CartTest.java
+++ AimsProject/src/hust/soict/globalict/aims/cart/CartTest.java
@@ -1,14 +1,16 @@
 package AimsProject.src.hust.soict.globalict.aims.cart;
+import AimsProject.src.hust.soict.globalict.aims.media.Book;
+import AimsProject.src.hust.soict.globalict.aims.media.CompactDisc;
 import AimsProject.src.hust.soict.globalict.aims.media.DigitalVideoDisc;
 public class CartTest {
     public static void main(String args[]){
         Cart cart = new Cart();
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-        cart.addDigitalVideoDisc(dvd1);
+        cart.addMedia(dvd1);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-        cart.addDigitalVideoDisc(dvd2);
+        cart.addMedia(dvd2);
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-        cart.addDigitalVideoDisc(dvd3);
+        cart.addMedia(dvd3);
         cart.print();
         cart.searchDVD(0);
         cart.searchDVD(1);
@@ -18,5 +20,10 @@ public class CartTest {
         cart.searchDVD("Ar");
         cart.searchDVD("zsw");
         cart.searchDVD("tar");
+        CompactDisc cd1 = new CompactDisc("Me", "Not me", "Category", 45.2f);
+        cart.addMedia(cd1);
+        Book book1 = new Book();
+        cart.addMedia(book1);
+        cart.print();
     }
 }

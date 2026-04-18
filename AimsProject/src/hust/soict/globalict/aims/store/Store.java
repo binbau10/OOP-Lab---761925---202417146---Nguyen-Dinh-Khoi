5f679b038b2e7784006ee9ccac60e367583b25cf
binbau10 <khoind2006@gmail.com>
2026-04-18 20:30:00 +0700

    Update Store class to work for Media class

--- AimsProject/src/hust/soict/globalict/aims/store/Store.java
+++ AimsProject/src/hust/soict/globalict/aims/store/Store.java
@@ -1,45 +1,24 @@
 package AimsProject.src.hust.soict.globalict.aims.store;
-import AimsProject.src.hust.soict.globalict.aims.media.DigitalVideoDisc;
+import AimsProject.src.hust.soict.globalict.aims.media.Media;
+import java.util.ArrayList;
 public class Store {
     public static final int MAX_NUMBERS_DVDS = 100;
-    private DigitalVideoDisc itemsInStore[] = new DigitalVideoDisc[MAX_NUMBERS_DVDS];
-    private int numberOfDVDs;
+    private ArrayList<Media> itemsInStore = new ArrayList<Media>();
     public Store(){
         super();
-        numberOfDVDs = 0;
     }
-    public void addDVD(DigitalVideoDisc disc){
-        if (numberOfDVDs >= MAX_NUMBERS_DVDS){
-            System.out.println("The store is full!");
+    public void addMedia(Media media){
+        if (itemsInStore.add(media)){
+            System.out.println("Media successfully added");
             return;
         }
-        itemsInStore[numberOfDVDs] = disc;
-        numberOfDVDs ++;
-        System.out.println("The disc is successfully added.");
-        if (numberOfDVDs >= MAX_NUMBERS_DVDS - 3){
-            System.out.println("The store is almost full!");
-        }
-        return;
+        System.out.println("Failed to add the media.");
     }
-    public void removeDVD(DigitalVideoDisc disc){
-        int removedDisc = 0;
-        for (int i = 0; i < numberOfDVDs; i++){
-            if (itemsInStore[i].equals(disc)){
-                removedDisc ++;
-            }
-            else{
-                itemsInStore[i - removedDisc] = itemsInStore[i];
-            }
+    public void removeMedia(Media media){
+        if (itemsInStore.remove(media)){
+            System.out.println("Media successfully removed");
+            return;
         }
-        numberOfDVDs -= removedDisc;
-        if (removedDisc == 0){
-            System.out.println("There is no DVDs in the store matching the input.");
-        }
-        else if (removedDisc == 1){
-            System.out.println("All " + removedDisc + " disc is now removed.");
-        }
-        else{
-            System.out.println("All " + removedDisc + " discs are now removed.");
-        }
+        System.out.println("Failed to remove the media.");
     }
 }

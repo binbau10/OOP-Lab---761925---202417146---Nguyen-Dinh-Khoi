a9ea1c86c1f85c637bd583c5474db8202a45ea7d
binbau10 <khoind2006@gmail.com>
2026-06-09 22:16:22 +0700

    Add practice exercise

--- GUIProject/hust/soict/globalict/javafx/PainterController.java
+++ GUIProject/hust/soict/globalict/javafx/PainterController.java
@@ -2,6 +2,8 @@ package GUIProject.hust.soict.globalict.javafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -11,7 +13,8 @@ public class PainterController {
 
     @FXML
     private Pane drawingAreaPane;
-
+    @FXML
+    private ToggleGroup penEraserToggleGroup;
     @FXML
     void clearButtonPressed(ActionEvent event) {
         drawingAreaPane.getChildren().clear();
@@ -19,8 +22,19 @@ public class PainterController {
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-        Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
-        drawingAreaPane.getChildren().add(newCircle);
+        RadioButton button = (RadioButton)penEraserToggleGroup.getSelectedToggle();
+        if (button == null){
+            System.out.println("Null");
+            return;
+        }
+        else if (button.getText().equals("Pen")){
+            Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
+            drawingAreaPane.getChildren().add(newCircle);
+        }
+        else{
+            Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.WHITE);
+            drawingAreaPane.getChildren().add(newCircle);
+        }
     }
 
 }
